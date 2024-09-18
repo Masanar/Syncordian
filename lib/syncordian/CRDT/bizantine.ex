4ba3a5e160afe88953651090c71e2cd561a51243
Mateo Sanabria Ardila <mateo.sanabria.a@gmail.com>
2024-09-17 21:09:00 -0500

    Improve some documentation && stop the mix phx.server terminal from printing debug information from the web interface

--- lib/syncordian/CRDT/bizantine.ex
+++ lib/syncordian/CRDT/bizantine.ex
@@ -7,6 +7,11 @@ defmodule Syncordian.Byzantine do
   """
   import Syncordian.Line_Object
 
+  @doc """
+    This function checks if the delete signature is valid by comparing it with the
+    signature created by the left parent and right parent content. Because the delete
+    signature is created in such manner.
+  """
   @spec check_signature_delete(
           deleted_line_signature :: Syncordian.Basic_Types.signature(),
           left_parent :: Syncordian.Line_Object.line(),
@@ -22,9 +27,9 @@ defmodule Syncordian.Byzantine do
   end
 
   @doc """
-    As a security feature the signature the creation of the signature for the delete operation
-    is different from the insert operation. This function, create a signature just by using
-    the left parent and right parent signatures.
+    As a security feature the signature the creation of the signature for the delete
+    operation is different from the insert operation. This function, create a signature
+    just by using the left parent and right parent signatures.
   """
   @spec create_signature_delete(
           left_parent :: Syncordian.Line_Object.line(),
@@ -39,8 +44,9 @@ defmodule Syncordian.Byzantine do
   end
 
   @doc """
-    This function checks if the signature is valid by comparing it with the signature created
-    by the left parent content, the new content, the right parent content and the peer id.
+    This function checks if the signature is valid by comparing it with the signature
+    created by the left parent content, the new content, the right parent content and the
+    peer id.
   """
   @spec check_signature_insert(
           left_parent :: Syncordian.Line_Object.line(),

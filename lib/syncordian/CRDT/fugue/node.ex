057677777a12c2fae7585805121ee4098d448602
mateo <mateo.sanabria.a@gmail.com>
2025-03-15 16:41:42 -0500

    First version of insert

--- lib/syncordian/CRDT/fugue/node.ex
+++ lib/syncordian/CRDT/fugue/node.ex
@@ -1,24 +1,25 @@
 defmodule Syncordian.Fugue.Node do
-  @null_id {"", 0}
-  @tombstone :tombstone
-
-  @type nodeID :: {String.t(), integer()}
-  @type treeSide :: :left | :right | nil
-  @type nodeValue :: String.t() | :tombstone
+  @null_id     {"", 0}
+  @left_value  :left
+  @right_value :right
+  @tombstone   :tombstone
+  @type node_ID :: {String.t(), integer()}
+  @type tree_side :: :left | :right | nil
+  @type node_value :: String.t() | :tombstone
 
   defstruct id: @null_id, value: @tombstone, parent: @null_id, side: nil
 
   @type t :: %__MODULE__{
-          id: nodeID,
-          value: nodeValue,
-          parent: nodeID,
-          side: treeSide
+          id: node_ID,
+          value: node_value,
+          parent: node_ID,
+          side: tree_side
         }
 
   @spec root() :: t
   def root(), do: %__MODULE__{}
 
-  @spec new(nodeID, nodeValue, nodeID, treeSide) :: t
+  @spec new(node_ID, node_value, node_ID, tree_side) :: t
   def new(id, value, parent, side),
     do: %__MODULE__{id: id, value: value, parent: parent, side: side}
 
@@ -28,35 +29,42 @@ defmodule Syncordian.Fugue.Node do
   @spec is_leaf?(t) :: boolean()
   def is_leaf?(node), do: node.id != @null_id
 
-  @spec get_null_id() :: nodeID
+  @spec get_null_id() :: node_ID
   def get_null_id(), do: @null_id
 
-  @spec get_tombstone() :: nodeValue
+  @spec get_tombstone() :: node_value
   def get_tombstone(), do: @tombstone
 
   # Getters
-  @spec get_id(t) :: nodeID
+  @spec get_id(t) :: node_ID
   def get_id(%__MODULE__{id: id}), do: id
 
-  @spec get_value(t) :: nodeValue
+  @spec get_value(t) :: node_value
   def get_value(%__MODULE__{value: value}), do: value
 
-  @spec get_parent(t) :: nodeID
+  @spec get_parent(t) :: node_ID
   def get_parent(%__MODULE__{parent: parent}), do: parent
 
-  @spec get_side(t) :: treeSide
+  @spec get_side(t) :: tree_side
   def get_side(%__MODULE__{side: side}), do: side
 
+  @spec get_right_value() :: tree_side()
+  def get_right_value(), do: @right_value
+
+  @spec get_left_value() :: tree_side()
+  def get_left_value(), do: @left_value
+
   # Setters
-  @spec set_id(t, nodeID) :: t
+  @spec set_id(t, node_ID) :: t
   def set_id(%__MODULE__{} = node, id), do: %__MODULE__{node | id: id}
 
-  @spec set_value(t, nodeValue) :: t
+  @spec set_value(t, node_value) :: t
   def set_value(%__MODULE__{} = node, value), do: %__MODULE__{node | value: value}
 
-  @spec set_parent(t, nodeID) :: t
+  @spec set_parent(t, node_ID) :: t
   def set_parent(%__MODULE__{} = node, parent), do: %__MODULE__{node | parent: parent}
 
-  @spec set_side(t, treeSide) :: t
+  @spec set_side(t, tree_side) :: t
   def set_side(%__MODULE__{} = node, side), do: %__MODULE__{node | side: side}
+
 end
--- lib/syncordian/CRDT/fugue/tree.ex
+++ lib/syncordian/CRDT/fugue/tree.ex
@@ -1,15 +1,19 @@
 defmodule Syncordian.Fugue.Tree do
   alias Syncordian.Fugue.Node
+  import Syncordian.Basic_Types
 
   defstruct nodes: %{}
-  @type nodeValue_list :: [Node.nodeValue()]
-  @type nodeID_List :: [Node.nodeID()]
-  @type node_entry :: {Node.t(), nodeID_List(), nodeID_List()}
-  @type t :: %__MODULE__{
-          nodes: %{Node.nodeID() => node_entry}
-        }
 
-  @spec new() :: t
+  @type node_fugue :: Node.t()
+  @type nodeValue_list :: [Node.node_value()]
+  @type nodeID_List :: [Node.node_ID()]
+  @type node_entry :: {node_fugue(), nodeID_List(), nodeID_List()}
+  @type t :: %__MODULE__{
+          nodes: %{Node.node_ID() => node_entry}
+        }
+  @type tree :: t()
+
+  @spec new() :: tree()
   def new do
     root = Node.root()
 
@@ -18,26 +22,31 @@ defmodule Syncordian.Fugue.Tree do
     }
   end
 
-  @spec values(t) :: nodeValue_list()
+  @spec get_root(tree()) :: node_entry | nil
+  def get_root(tree), do: Map.get(tree.nodes, Node.get_null_id())
+
+  @spec values(tree()) :: [node_fugue()]
   def values(tree), do: traverse(tree)
 
-  @spec traverse(t, Node.nodeID()) :: nodeValue_list()
-  def traverse(tree, id \\ Node.get_null_id()) do
-    traverse_acc_reduce =
-      fn id, acc
-        -> traverse(tree, id) ++ acc
-      end
-    recursion =
-      fn nodeId_List
-        -> Enum.reduce(nodeId_List, [], traverse_acc_reduce)
-      end
+  @spec traverse(tree()) :: [node_fugue()]
+  def traverse(tree), do: traverse(tree, Node.get_null_id())
+
+  @spec traverse(tree(), Node.node_ID(), boolean()) :: [node_fugue()]
+  def traverse(tree, id, tombstone \\ false) do
+    traverse_acc_reduce = fn id, acc ->
+      traverse(tree, id) ++ acc
+    end
+
+    recursion = fn nodeId_List ->
+      Enum.reduce(nodeId_List, [], traverse_acc_reduce)
+    end
 
     case Map.get(tree.nodes, id) do
       {node, left, right} ->
         left_values = recursion.(left)
 
         node_value =
-          if Node.get_value(node) != Node.get_tombstone(),
+          if (Node.get_value(node) != Node.get_tombstone()) or (not tombstone),
             do: [Node.get_value(node)],
             else: []
 
@@ -45,9 +54,49 @@ defmodule Syncordian.Fugue.Tree do
 
         left_values ++ node_value ++ right_values
 
-      nil -> []
+      nil ->
+        []
+    end
+  end
+
+  @spec right_child_exists?(tree(), Node.node_ID()) :: boolean()
+  defp right_child_exists?(tree, id) do
+    Enum.any?(tree.nodes, fn
+      {_id, {node, _left, _right}} ->
+        Node.get_parent(node) == id and Node.get_side(node) == Node.get_right_value()
+    end)
+  end
+
+  @spec insert(tree(), String.t(), integer(), integer(), Node.node_value()) :: node_fugue()
+  def insert(tree, replica_id, counter, position, value) do
+    id = {replica_id, counter}
+
+    left_origin =
+      if position == 0 do
+        get_root(tree)
+      else
+        tree
+        |> traverse()
+        |> Enum.at(position - 1)
+      end
+
+    left_origin_id = Node.get_id(left_origin)
+
+    # TODO: Review the third argument. Should it be root or left_origin_id?
+    #       Additionally, check if the 0 position is actually the left_origin node.
+    right_origin =
+      traverse(tree, left_origin_id, true)
+      |> Enum.at(1)
+
+    case right_child_exists?(tree, left_origin_id) do
+      false ->
+        Node.new(id, value, left_origin_id, Node.get_right_value())
+      true ->
+        Node.new(id, value, right_origin, Node.get_left_value())
     end
 
   end
 
+
+  # THE END
 end
